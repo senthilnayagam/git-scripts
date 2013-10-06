fd673406b0048000c536b1a42536657fa8172526
Senthil Nayagam <senthil@railsfactory.org>
2013-10-06 10:20:55 -0400

    list file by lines of code

---
+++ git-list-files-by-lines.sh
@@ -0,0 +1,4 @@
+
+# I have a large repo where xargs, and most common commands fail with "Argument list too long"
+# replacement is parallel
+ git --no-pager grep -I --name-only '' | parallel wc -l | sort -n
